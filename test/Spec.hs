a12649c1952ea01e7da08e0357cd02a1dcf3db15
jdcarbeck <jdcarbeck@gmail.com>
2018-10-24 15:56:37 +0100

    finished adding tests into prior implementation

--- test/Spec.hs
+++ test/Spec.hs
@@ -19,47 +19,56 @@ testTree = Node 4
     (Node 7 Empty Empty))
 
 tests :: [TF.Test]
-tests = [testGroup "\n\nTesting for DAGS"
-          [creationOfTreeFromList
+tests = [testGroup "\n\nTesting for LCA"
+          [ creationOfTreeFromList
+          , pathFromGivenTree
+          , commonPathInGivenTree
           ]
         ]
 
 creationOfTreeFromList :: TF.Test
 creationOfTreeFromList
  = testGroup "\nTree creation from a list"
-    [ testCase "Creates an empty tree with 0 element list"
-        ( createTree [] @?= Empty (Empty Empty)
-    , testCase "Creates a tree with 1 element list"
+    [ testCase "Creates a tree with 1 element list"
         ( lca 1 1 (createTree [1]) @?= 1 )
     , testCase "Creates a tree with 2 element list"
         ( lca 1 2 (createTree [1,2]) @?= 2 )
+    , testCase "Creates a tree with 3 element list"
+       ( lca 1 3 (createTree [1,3,2]) @?= 2 )
     ]
 
+pathFromGivenTree :: TF.Test
+pathFromGivenTree
+ = testGroup "\nPaths of node given in the sample balenced tree"
+    [ testCase "Path of Node 1"
+        ( findPath 1 testTree @?= [4,2,1] )
+    , testCase "Path of Node 2"
+        ( findPath 2 testTree @?= [4,2] )
+    , testCase "Path of Node 3"
+        ( findPath 3 testTree @?= [4,2,3] )
+    , testCase "Path of Node 4"
+        ( findPath 4 testTree @?= [4] )
+    , testCase "Path of Node 5"
+        ( findPath 5 testTree @?= [4,6,5] )
+    , testCase "Path of Node 6"
+        ( findPath 6 testTree @?= [4,6] )
+    , testCase "Path of Node 7"
+        ( findPath 7 testTree @?= [4,6,7] )
+    ]
 
-    -- describe "find path from test in Spec.hs" $ do
-    --   it "returns the path from 1 testTree" $
-    --     findPath 1 testTree `shouldBe` [4,2,1]
-    --   it "returns the path from 2 testTree" $
-    --     findPath 2 testTree `shouldBe` ([4,2])
-    --   it "returns the path from 3 testTree" $
-    --     findPath 3 testTree `shouldBe` ([4,2,3])
-    --   it "returns the path from 4 testTree" $
-    --     findPath 4 testTree `shouldBe` ([4])
-    --   it "returns the path from 5 testTree" $
-    --     findPath 5 testTree `shouldBe` ([4,6,5])
-    --   it "returns the path from 6 testTree" $
-    --     findPath 6 testTree `shouldBe` ([4,6])
-    --   it "returns the path from 7 testTree" $
-    --     findPath 7 testTree `shouldBe` ([4,6,7])
-    -- describe "common path from Tree.hs" $ do
-    --   it "returns the empty list for no common path" $
-    --     commonPath [1,4,5] [] `shouldBe` []
-    --   it "returns the root in oposite leaf nodes" $
-    --     commonPath (findPath 7 testTree) (findPath 3 testTree) `shouldBe` [4]
-    --   it "returns the parent in a parent and child node" $
-    --     commonPath (findPath 2 testTree) (findPath 1 testTree) `shouldBe` [4,2]
-    --   it "returns the parent for two child nodes" $
-    --     commonPath (findPath 3 testTree) (findPath 1 testTree) `shouldBe` [4,2]
+commonPathInGivenTree :: TF.Test
+commonPathInGivenTree
+ = testGroup "\nCommon paths of given nodes in sample balenced tree"
+    [ testCase "CommonPath of elems without a common path"
+        ( commonPath [1,4,5] [] @?= [] )
+    , testCase "CommonPath of two leaf nodes"
+        ( commonPath (findPath 7 testTree) (findPath 3 testTree) @?= [4] )
+    , testCase "CommonPath of child and parent"
+        ( commonPath (findPath 2 testTree) (findPath 1 testTree) @?= [4,2] )
+    , testCase "CommonPath of two child node with same parent"
+        ( commonPath (findPath 3 testTree) (findPath 1 testTree) @?= [4,2] )
+    ]
+
     -- describe "lca from testTree in Spec.hs" $ do
     --   it "returns correct lca for 1 2 testTree" $
     --     lca 1 2 testTree `shouldBe` 2
